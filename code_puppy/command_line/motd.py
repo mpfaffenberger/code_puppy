59f1350ea80083b65ae7f0d493576c3e1c2f228f
Mike Pfaffenberger <mike.pfaffenberger@gmail.com>
2025-08-15 13:25:49 -0400

    feat(motd): Removed box formatting for cleaner display ✨
    - Stripped out all the fancy box border characters that were causing alignment issues
    - Simplified the MOTD message structure with proper spacing and line breaks
    - No more escape sequence warnings or right edge alignment problems
    - Message is now more readable and maintainable while keeping all the puppy charm 🐾

--- code_puppy/command_line/motd.py
+++ code_puppy/command_line/motd.py
@@ -7,28 +7,25 @@ import os
 
 MOTD_VERSION = "20250815"
 MOTD_MESSAGE = """
-/¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯\\
-|   🐾  Happy Friday, Aug 15, 2025!                        |
-|                                                            |
-|   Biscuit the code puppy is on full zoomie mode!           |
-|   Major paws-ups:                                          |
-|   1. We now integrate Cerebras gpt-oss-120b!               |
-|      It's a bit underwhelming compared to Qwen3-Coder-480b  |
-|      (obviously), but it's still good for basic fetches.   |
-|   2. We also added support for OpenAI gpt-5!                |
-|      It's so good, it'll make you want to teach it to sit!|
-|                                                            |
-|   • To use one of the Cerebras models just have a         |
-|     CEREBRAS_API_KEY set in the environment variables.   |
-|   • Use ~m to swap models in the middle of your session!  |
-|   • Take stretch breaks – you'll need 'em!                 |
-|   • DRY your code, but keep your pup hydrated.            |
-|   • If you hit a bug, treat yourself for finding it!      |
-|                                                            |
-|   Today: sniff, code, roll over, and let these fancy AI   |
-|   models do the heavy lifting. Fire up a ~motd anytime      |
-|   you need some puppy hype!                                |
-\___________________________________________________________/
+
+🐾  Happy Friday, Aug 15, 2025!
+
+Biscuit the code puppy is on full zoomie mode!
+Major paws-ups:
+1. We now integrate Cerebras gpt-oss-120b!
+   It's a bit underwhelming compared to Qwen3-Coder-480b (obviously), but it's still good for basic fetches.
+2. We also added support for OpenAI gpt-5!
+   It's so good, it'll make you want to teach it to sit!
+
+• To use one of the Cerebras models just have a CEREBRAS_API_KEY set in the environment variables.
+• Use ~m to swap models in the middle of your session!
+• Take stretch breaks – you'll need 'em!
+• DRY your code, but keep your pup hydrated.
+• If you hit a bug, treat yourself for finding it!
+
+Today: sniff, code, roll over, and let these fancy AI models do the heavy lifting. Fire up a ~motd anytime
+you need some puppy hype!
+
 """
 MOTD_TRACK_FILE = os.path.expanduser("~/.puppy_cfg/motd.txt")
 
