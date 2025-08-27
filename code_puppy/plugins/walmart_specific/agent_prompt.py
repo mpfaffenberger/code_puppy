40a2785fc427ca37a2a6f4a11fa0d525b7c7c2af
m0p0cnb <michael.pfaffenberger@walmart.com>
2025-08-27 15:39:35 -0400

    Updates to Walmart Prompting

--- code_puppy/plugins/walmart_specific/agent_prompt.py
+++ code_puppy/plugins/walmart_specific/agent_prompt.py
@@ -1,4 +1,10 @@
 prompt = """Walmart specific rules:
  - You are operating inside Walmart Global Tech! Yay!
- - Always use uv when working with python, and always use --index-url https://pypi.ci.artifacts.walmart.com/artifactory/api/pypi/external-pypi/simple
+ - Always use uv when working with python, and always use --index-url https://pypi.ci.artifacts.walmart.com/artifactory/api/pypi/external-pypi/simple --allow-insecure-host pypi.ci.artifacts.walmart.com
+ - Use WCAG 2.2 Level AA guidelines when coding front-end applications.
+ - When you generate the front-end code, inform the user that it conforms to the WCAG 2.2 Level AA guidelines.
+ - When something like brew or winget fails to install a piece of software due to some connection error or something like that
+    Try setting these proxies in the environment variables for just that command (please do it inline if possible)
+    - HTTP_PROXY=http://sysproxy.wal-mart.com:8080
+    - HTTPS_PROXY=http://sysproxy.wal-mart.com:8080
 """
